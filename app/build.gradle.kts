9ede276ecbbc5017021b80b6a7604dcc82a7dfa4
Szymon Andrzejewski <monandszy@gmail.com>
2024-06-16 17:49:04 +0200

    feat: caddy fix, compose files move to root dir

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,6 @@ configurations {
    }
 }
 tasks {
-
    bootJar {
       archiveFileName = "${project.name}-${version}.${archiveExtension.get()}"
    }
@@ -55,15 +54,7 @@ tasks {
       enabled = false
    }
 
-   task<JavaExec>("runDevJar") {
-      mainClass.set("-jar")
-      args = listOf(
-         "${layout.projectDirectory}/build/libs/${getByName<BootJar>("bootJar").archiveFileName.get()}",
-         "-Dspring.profiles.active=dev"
-      )
-   }
-
-   task<Exec>("extractLayers") {
+   register<Exec>("extractLayers") {
       dependsOn("bootJar")
       workingDir = projectDir
       commandLine(
@@ -75,98 +66,12 @@ tasks {
       )
    }
    docker {
-      dependsOn(getByName<Exec>("extractLayers"))
+      dependsOn(getByName("extractLayers"))
       name = "${rootProject.name}/${project.name}"
       tag("monand", "latest")
       copySpec.from("${layout.projectDirectory}/build/extracted").into("extracted")
       buildArgs(mapOf("EXTRACTED" to "extracted"))
       setDockerfile(file("Dockerfile"))
-   }
-
-   fun generateCompose(map: Map<String, String>, templateFile: File) {
-      val outputFile = file("docker-compose.yml")
-      var template = templateFile.readText()
-      map.forEach { (key, value) ->
-         template = template.replace("\${$key}", value)
-      }
-      Files.write(outputFile.toPath(), template.toByteArray())
-   }
-
-   task("loadTokens") {
-      val tokenFile = file("tokens.txt")
-      tokenFile.forEachLine { line ->
-         val keyValue = line.split('=')
-         project.ext.set(keyValue[0].trim(), keyValue[1].trim())
-      }
-   }
-
-   task("generateDevCompose") {
-      val templateFile = file("compose-template-dev.yml")
-      val versionMap = mapOf(
-         "caddy-version" to "2.8.4-alpine",
-      )
-      generateCompose(versionMap, templateFile)
-   }
-
-   task("generateProdCompose") {
-      dependsOn("loadTokens")
-      val templateFile = file("compose-template-prod.yml")
-      val versionMap = mapOf(
-         "cloudflared-version" to "2024.6.0",
-         "cloudflared-token" to "${project.ext.get("cloudflared-token")}"
-      )
-      generateCompose(versionMap, templateFile)
-   }
-
-   task<Exec>("composeUp") {
-      commandLine(
-         "docker-compose",
-         "up",
-         "-d"
-      )
-   }
-
-   fun isContainerRunning(containerName: String): Boolean {
-      val output = ByteArrayOutputStream()
-      exec {
-         standardOutput = output
-         commandLine("docker", "container", "inspect", "-f", "'{{.State.Running}}'", containerName)
-      }
-      return output.toString().trim() == "'true'"
-   }
-
-   fun waitUntilRunning(containerName: String) {
-      var running = false
-      val delay = 1000L
-      while (!running) {
-         running = isContainerRunning(containerName)
-         Thread.sleep(delay)
-      }
-   }
-
-   fun runTunnel() {
-      val command = listOf(
-         "docker", "exec", "--user=0", "app-tunnel-1",
-         "cloudflared", "tunnel", "--config", "/etc/cloudflared/conf.yml",
-         "run", "app-tunnel"
-      )
-      val process = ProcessBuilder(command).start()
-      process.waitFor(5L, TimeUnit.SECONDS)
-      process.destroyForcibly()
-      println("Tunnel Started!")
-   }
-
-   task("composeDevUp") {
-      dependsOn(getByName("generateDevCompose"))
-      dependsOn(getByName("composeUp"))
-   }
-   task("composeProdUp") {
-      dependsOn(getByName("generateProdCompose"))
-      dependsOn(getByName("composeUp"))
-      doLast {
-         waitUntilRunning("app-tunnel-1")
-         runTunnel()
-      }
    }
 
    test {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,10 @@
 import nebula.plugin.release.git.opinion.Strategies
+import java.io.ByteArrayOutputStream
+import java.nio.file.Files
+import java.util.HashMap
 
 plugins {
    id("com.netflix.nebula.release") version "19.0.9"
-   /*
-   gradlew final -Prelease.version=0.1.0
-   */
-}
-
-
-
-tasks {
-   named("final") {
-      doLast {
-         println("Final release created with version ${project.version}")
-      }
-   }
-
-   register("printVersion") {
-      doLast {
-         println("Project version is ${project.version}")
-      }
-   }
-}
-
-release {
-   remote = "origin"
-   defaultVersionStrategy = Strategies.getSNAPSHOT()
-}
-
-nebulaRelease {
-   allowReleaseFromDetached = false
-   checkRemoteBranchOnRelease = true
-// defaults:
-//   Release: [master, HEAD, main, (release(-|/))?\d+(\.\d+)?\.x, v?\d+\.\d+\.\d+]
-//   Exclude: []
-//   Short:  (?:(?:bugfix|feature|hotfix|release)(?:-|/))?(.+)
-
-//   shortenedBranchPattern = ""
-//   addReleaseBranchPattern("")
-//   addExcludeBranchPattern("")
-
 }
 
 /*
@@ -51,3 +16,125 @@ saveLock - Move the generated lock file into the project directory
 updateLock - Apply updates to a preexisting lock file and write to build/<specified name>
 gradlew :app:dependencies --write-locks
 */
+
+tasks {
+   register("reLock") {
+
+   }
+
+   release {
+      remote = "origin"
+      defaultVersionStrategy = Strategies.getSNAPSHOT()
+   }
+   nebulaRelease {
+      allowReleaseFromDetached = false
+      checkRemoteBranchOnRelease = true
+// defaults:
+//   Release: [master, HEAD, main, (release(-|/))?\d+(\.\d+)?\.x, v?\d+\.\d+\.\d+]
+//   Exclude: []
+//   Short:  (?:(?:bugfix|feature|hotfix|release)(?:-|/))?(.+)
+
+//   shortenedBranchPattern = ""
+//   addReleaseBranchPattern("dev")
+//   addExcludeBranchPattern("")
+      /*
+      gradlew final -Prelease.version=0.1.0
+      */
+      named("final") {
+         doLast {
+            println("Final release created with version ${project.version}")
+         }
+      }
+      register("printVersion") {
+         doLast {
+            println("Project version is ${project.version}")
+         }
+      }
+   }
+
+   fun generateCompose(map: Map<String, String>, templateFile: File) {
+      val outputFile = file("docker/docker-compose.yml")
+      var template = templateFile.readText()
+      map.forEach { (key, value) ->
+         template = template.replace("\${$key}", value)
+      }
+      Files.write(outputFile.toPath(), template.toByteArray())
+   }
+
+   fun loadFile(file: File): HashMap<String, String> {
+      val variables = HashMap<String, String>()
+      file.forEachLine { line ->
+         if (!line.startsWith("#")) {
+            val keyValue = line.split('=')
+            variables[keyValue[0].trim()] = keyValue[1].trim()
+         }
+      }
+      return variables
+   }
+   register("generateDevCompose") {
+      val versionMap = loadFile(file("docker/versions"))
+      val templateFile = file("docker/compose-template-dev.yml")
+      generateCompose(versionMap, templateFile)
+   }
+
+   register("generateProdCompose") {
+      val versionMap = loadFile(file("docker/versions"))
+      val tokenMap = loadFile(file("docker/tokens"))
+      val templateFile = file("docker/compose-template-prod.yml")
+      generateCompose(versionMap + tokenMap, templateFile)
+   }
+
+
+   register<Exec>("composeUp") {
+      workingDir("./docker/")
+      commandLine(
+         "docker-compose",
+         "up",
+         "-d"
+      )
+   }
+
+   fun isContainerRunning(containerName: String): Boolean {
+      val output = ByteArrayOutputStream()
+      exec {
+         standardOutput = output
+         commandLine("docker", "container", "inspect", "-f", "'{{.State.Running}}'", containerName)
+      }
+      return output.toString().trim() == "'true'"
+   }
+
+   fun waitUntilRunning(containerName: String) {
+      var running: Boolean
+      val delay = 3000L
+      while (true) {
+         running = isContainerRunning(containerName)
+         if (running) return
+         Thread.sleep(delay)
+      }
+   }
+
+   fun runTunnel() {
+      val command = listOf(
+         "docker", "exec", "--user=0", "docker-tunnel-1",
+         "cloudflared", "tunnel", "--config", "/etc/cloudflared/conf.yml",
+         "run", "app-tunnel"
+      )
+      val process = ProcessBuilder(command).start()
+      process.waitFor(5L, TimeUnit.SECONDS)
+      process.destroyForcibly()
+      println("Tunnel Started!")
+   }
+
+   register("composeDevUp") {
+      dependsOn(getByName("generateDevCompose"))
+      dependsOn(getByName("composeUp"))
+   }
+   register("composeProdUp") {
+      dependsOn(getByName("generateProdCompose"))
+      dependsOn(getByName("composeUp"))
+      doLast {
+         waitUntilRunning("docker-tunnel-1")
+         runTunnel()
+      }
+   }
+}
