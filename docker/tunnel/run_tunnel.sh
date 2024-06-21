0b6e2d8bf2f06f37eaae89a792c53941bafae61a
Monand <monandszy@tuta.io>
2024-06-21 22:23:42 +0200

    feat: tunnel run update?

--- docker/tunnel/run_tunnel.sh
+++ docker/tunnel/run_tunnel.sh
@@ -1,5 +1,5 @@
 #!/run/current-system/sw/bin/bash
 
-docker exec --user=0 prod-tunnel-1 \
+docker exec -e TUNNEL_ORIGIN_CERT=/etc/cloudflared/cert.pem --user=0 prod-tunnel-1 \
     cloudflared tunnel --config /etc/cloudflared/conf.yml \
     run app-tunnel
