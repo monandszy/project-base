175b955b0fbc1bf6e3b4728963f72c531f36a6f3
Monand <monandszy@tuta.io>
2024-06-22 20:06:31 +0200

    fix:  log

--- devUp.sh
+++ devUp.sh
@@ -1,4 +1,4 @@
 #!/run/current-system/sw/bin/bash
 LOG_FILE=./gradle/dev.log
 
-gradle composeDevUp $LOG_FILE 2>&1
+gradle composeDevUp >> $LOG_FILE 2>&1
--- prodUp.sh
+++ prodUp.sh
@@ -2,7 +2,7 @@
 LOG_FILE=./gradle/prod.log
 
 bash docker/tunnel/clean_ports.sh $LOG_FILE 2>&1
-gradle composeProdUp $LOG_FILE 2>&1
+gradle composeProdUp >> $LOG_FILE 2>&1
 bash docker/tunnel/run_tunnel.sh &
 sleep 10 & pid=$!
 kill $pid
