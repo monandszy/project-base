0a9f5f757a91969028b8d18f6222aa08938caef5
Monand <monandszy@tuta.io>
2024-09-11 17:00:50 +0000

    feat: minor refactor

--- prodUp.sh
+++
@@ -1,8 +0,0 @@
-#!/run/current-system/sw/bin/bash
-LOG_FILE=./gradle/prod.log
-
-bash docker/tunnel/clean_ports.sh $LOG_FILE 2>&1
-gradle composeProdUp >> $LOG_FILE 2>&1
-bash docker/tunnel/run_tunnel.sh &
-sleep 10 & pid=$!
-kill $pid
