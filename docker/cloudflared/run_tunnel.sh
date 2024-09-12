6eb7be1257b01764fd352e4104c5380b65918685
Monand <monandszy@tuta.io>
2024-09-12 15:49:40 +0200

    fix: tunnel script

--- docker/cloudflared/run_tunnel.sh
+++ docker/cloudflared/run_tunnel.sh
@@ -1,7 +1,7 @@
 #!/run/current-system/sw/bin/bash
 
-docker exec -e TUNNEL_ORIGIN_CERT=/etc/cloudflared/cert.pem \
+docker exec -e TUNNEL_ORIGIN_CERT=/root/.cloudflared/cert.pem \
     --user=0 networking-cloudflared \
     cloudflared tunnel \
     --config /etc/cloudflared/conf.yml \
-    run app-tunnel
+    run app-tunnel-4
